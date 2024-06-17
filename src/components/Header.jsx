4adcd353202c5e43e5ac8586ca99d030f21942f9
webdev-ashishk <webdev.ashishk.com>
2024-06-17 15:55:28 +0530

    write logic for search functionality in newsapp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,25 @@ export default function Header() {
   const [searchText, setSearchText] = useState("");
   return (
     <div className="text-white">
-      <input
-        type="text"
-        placeholder="search..."
-        value={searchText}
-        onChange={(e) => {
-          setSearchText(e.target.value);
-        }}
-        className="text-white w-[300px] h-[50px] m-2 pl-5 text-2xl border-2 rounded-sm"
-      />
+      <nav className="flex gap-5 text-[20px] font-bold  justify-center">
+        {/* <ul className="">
+          <li>Logo</li>
+        </ul> */}
+        {/* <ul className="flex justify-center items-center gap-14">
+          <li>Home</li>
+          <li>about</li>
+          <li>Contact</li>
+        </ul> */}
+        <input
+          type="text"
+          placeholder="search..."
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+          className="text-white w-[400px] h-[100px] m-2 pl-5 text-2xl border-2 rounded-sm mr-16"
+        />
+      </nav>
     </div>
   );
 }
