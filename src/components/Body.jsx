3e9c717f807ac316995d0a8094532b5400051618
webdev-ashishk <webdev.ashishk.com>
2024-06-18 14:28:49 +0530

    fetched data even news.length is underfined or zero

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import NewsCard from "./NewsCard";
-import ShimmerUI from "./ShimmerUI";
 export default function Body() {
   const [allNews, setAllNews] = useState([]);
-  const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("apple");
+  const apiKey = process.env.REACT_APP_API_KEY;
   useEffect(() => {
     fetchNews(searchText);
   }, [searchText]);
   async function fetchNews(searchText) {
     const response = await fetch(
-      `https://newsapi.org/v2/everything/?q=${searchText}s&apiKey=2f7fbc902de14f53a875f1f00621b9f7`
+      `https://newsapi.org/v2/everything/?q=${searchText}s&apiKey=${apiKey}`
     );
     const json = await response.json();
     setAllNews(json.articles);
@@ -17,11 +17,39 @@ export default function Body() {
   }
 
   console.log(searchText);
-  if (allNews.length === 0) {
-    return <ShimmerUI />;
+  {
+    if (allNews.length === 0) {
+      return (
+        <div className="search-input text-center">
+          <input
+            type="text"
+            placeholder="search..."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            className="text-white w-[400px] h-[100px] m-2 pl-5 text-2xl border-2 rounded-sm mr-16"
+          />
+        </div>
+      );
+    } else if (allNews.length === undefined) {
+      return (
+        <div className="search-input text-center">
+          <input
+            type="text"
+            placeholder="search..."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            className="text-white w-[400px] h-[100px] m-2 pl-5 text-2xl border-2 rounded-sm mr-16"
+          />
+        </div>
+      );
+    }
   }
   return (
-    <div className="">
+    <div className="text-red-400">
       <div className="search-input text-center">
         <input
           type="text"
--- src/components/ShimmerUI.jsx
+++ src/components/ShimmerUI.jsx
@@ -1,10 +1,7 @@
 export default function ShimmerUI() {
   return (
-    <div className="flex flex-col gap-4 w-[300px] h-[350px] m-5">
-      <div className="skeleton h-32 w-full"></div>
-      <div className="skeleton h-4 w-28"></div>
-      <div className="skeleton h-4 w-full"></div>
-      <div className="skeleton h-4 w-full"></div>
-    </div>
+    <>
+      <p>search other query ... </p>
+    </>
   );
 }
